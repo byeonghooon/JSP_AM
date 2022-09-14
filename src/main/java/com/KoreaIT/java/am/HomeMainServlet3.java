a662f72205882c374d2633de2e984e47fde4db49
byeonghunki <orangeys1000@gmail.com>
2022-09-14 14:13:24 +0900

    파라미터에 따라서 텍스트의 색상 변경

--- src/main/java/com/KoreaIT/java/am/HomeMainServlet3.java
+++ src/main/java/com/KoreaIT/java/am/HomeMainServlet3.java
@@ -19,17 +19,22 @@ public class HomeMainServlet3 extends HttpServlet {
 		if (inputedDan == null) {
 			inputedDan = "1";
 		}
-		String inputedlimit = request.getParameter("limit");
-		if (inputedlimit == null) {
-			inputedlimit = "9";
+		String inputedLimit = request.getParameter("limit");
+		if (inputedLimit == null) {
+			inputedLimit = "9";
+		}
+		
+		String inputedColor = request.getParameter("color");
+		if (inputedColor == null) {
+			inputedColor = "black";
 		}
 
 		int dan = Integer.parseInt(inputedDan);
-		int limit = Integer.parseInt(inputedlimit);
-		response.getWriter().append(String.format("%d단<br>", dan));
+		int limit = Integer.parseInt(inputedLimit);
+		response.getWriter().append(String.format("<div style =\"color:%s;\">%d단</div>",inputedColor, dan));
 
 		for (int i = 1; i <= limit; i++) {
-			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+			response.getWriter().append(String.format("<div style =\"color:%s;\">%d * %d = %d</div>",inputedColor, dan, i, dan * i));
 		}
 
 	}
